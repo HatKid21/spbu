71ba80769281420fa78ada5d52d2b69d41c60ddd
HatKid21 <dimno2007@gmail.com>
2025-11-19 20:23:26 +0300

    Add 3rd level logic. Add ascii art

--- multi-level menu/menuFunctions.cpp
+++ multi-level menu/menuFunctions.cpp
@@ -3,6 +3,7 @@
 
 #include "menu.hpp"
 #include "menuFunctions.hpp"
+#include "artAssets.hpp"
 
 const hatkid::MenuItem* hatkid::showMenu(const hatkid::MenuItem* current){
 
@@ -20,10 +21,18 @@ const hatkid::MenuItem* hatkid::showMenu(const hatkid::MenuItem* current){
 }
 
 const hatkid::MenuItem* hatkid::differential(const MenuItem* current){
+	std::cout << "Бочкарёв Анатолий Олегович>";
+	std::cout << "Основные понятия в дифферециальном исчислении - производная и дифференциал" << std::endl;
+	std::cout << "Произваодная - это отношение приращения функции на приращение аргумента" << std::endl;
+	std::cout << "Дифференциал - это главная линейная часть приращения" << std::endl;
+	std::cout << std::endl;
 	return current->parent;
 }
 
 const hatkid::MenuItem* hatkid::integral(const MenuItem* current){
+	std::cout << "Бочкарёв Анатолий Олегович>";
+	std::cout << "А эта тема будет у нас на будущих лекциях. Не пропусти!" << std::endl;
+	std::cout << std::endl;
 	return current->parent;
 }
 
@@ -33,20 +42,34 @@ const hatkid::MenuItem* hatkid::subjectGoBack(const MenuItem* current){
 
 const hatkid::MenuItem* hatkid::add(const MenuItem* current){
 	std::cout << "Утешев Алексей Юрьевич>";
-	std::cout << "В общем случае у нас числа комплексные. " << std::endl;
+	std::cout << "В общем случае у нас числа комплексные." << std::endl;
 	std::cout << "Вот тебе пример: (1+2i) + (5-3i) = 6-i" << std::endl;
+	std::cout << std::endl;
 	return current->parent;
 }
 
 const hatkid::MenuItem* hatkid::subtract(const MenuItem* current){
+	std::cout << "Утешев Алексей Юрьевич>";
+	std::cout << "В общем случае у нас числа комплексные." << std::endl;
+	std::cout << "Вот тебе пример: (1+2i) - (5-3i) = -4+5i" << std::endl;
+	std::cout << std::endl;
 	return current->parent;
 }
 
 const hatkid::MenuItem* hatkid::multiply(const MenuItem* current){
+	std::cout << "Утешев Алексей Юрьевич>";
+	std::cout << "Там есть формула, но никто ее не запоминает. Легче как с обычными числами работать" << std::endl;
+	std::cout << "Вот тебе пример: (1+2i) * (5-3i) =5-3i+10i-6i^2=11+7i" << std::endl;
+	std::cout << std::endl;
 	return current->parent;
 }
 
 const hatkid::MenuItem* hatkid::divide(const MenuItem* current){
+	std::cout << "Утешев Алексей Юрьевич>";
+	std::cout << "А вот тут не все так просто. Если мы делим на комплекное число, надо домножить числитель и знаменатель на комплексно сопряженное." << std::endl;
+	std::cout << "Вот тебе пример:";
+	std::cout << "(1+2i) / (5-3i) = (-1+13i)/34=-1/34+13i/34" << std::endl;
+	std::cout << std::endl;
 	return current->parent;
 }
 
@@ -56,13 +79,17 @@ const hatkid::MenuItem* hatkid::exit(const MenuItem* current){
 }
 
 const hatkid::MenuItem* hatkid::studyAlgebra(const MenuItem* current){
-	std::cout << "Утешев Алексей Юрьевич> Критическая масса из 1 человека собралась...";
+	std::cout << hatkid::UAY;
+	std::cout << "Утешев Алексей Юрьевич>";
+	std::cout << "Чтож... критическая масса из 1-го человека собралась...";
 	std::cout << "Чему хочешь научиться сегодня?" << std::endl;
 	return showMenu(current);
 }
 
 const hatkid::MenuItem*  hatkid::studyMathAnalysis(const MenuItem* current){
-	std::cout << "Бочкарёв Анатолий Олегович> Здравствуй. Что ты сегодня хочешь повторить?" << std::endl;
+	std::cout << hatkid::BAO;
+	std::cout << "Бочкарёв Анатолий Олегович>";
+	std::cout << "Здравствуй. Чему ты хочешь сегодня научиться?" << std::endl;
 	return showMenu(current);
 }
 
