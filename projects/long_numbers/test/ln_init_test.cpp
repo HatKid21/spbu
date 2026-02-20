dcdbfed79ebd88ad4a124e2785d4261b9ea4ef3c
HatKid21 <dimno2007@gmail.com>
2026-02-20 14:40:02 +0300

    extend initialization test

--- projects/long_numbers/test/ln_init_test.cpp
+++ projects/long_numbers/test/ln_init_test.cpp
@@ -2,17 +2,56 @@
 
 #include "long_number.hpp"
 
-TEST(LongNumberConstructor, DefaultConstructor){
-    hatkid::LongNumber ln1;
+const hatkid::LongNumber ZERO{};
+const hatkid::LongNumber VAL_123{"123"};
+const hatkid::LongNumber VAL_123_NEG{"-123"};
+
+TEST(LongNumberInitialization, DefaultConstructor){
+   
+    hatkid::LongNumber def;
+
+    EXPECT_FALSE(def.is_negative());
+
+}
+
+TEST(LongNumberInitialization, StringConstructor){
     
-    EXPECT_FALSE(ln1.is_negative());
+    EXPECT_FALSE(VAL_123.is_negative());
+    EXPECT_TRUE(VAL_123_NEG.is_negative());
 
 }
 
-TEST(LongNumberConstructor, StringConstructor){
-    hatkid::LongNumber ln1("123");
-    hatkid::LongNumber ln2("-123");
+TEST(LongNumberInitialization, StringAssigment){
+    
+    hatkid::LongNumber test_zero = "0";
+    hatkid::LongNumber test_123 = "123";
+    hatkid::LongNumber test_123_neg = "-123";
 
-    EXPECT_FALSE(ln1.is_negative());
-    EXPECT_TRUE(ln2.is_negative());
+    EXPECT_EQ(test_zero, ZERO);
+    EXPECT_EQ(test_123, VAL_123);
+    EXPECT_EQ(test_123_neg, VAL_123_NEG);
+
 }
+
+TEST(LongNumberInitialization, CopyAssigment){
+
+    hatkid::LongNumber test_zero = ZERO;
+    hatkid::LongNumber test_123 = VAL_123;
+    hatkid::LongNumber test_123_neg = VAL_123_NEG;
+
+    EXPECT_EQ(test_zero, ZERO);
+    EXPECT_EQ(test_123, VAL_123);
+    EXPECT_EQ(test_123_neg, VAL_123_NEG);
+
+}
+
+TEST(LongNumberInitialization, MoveAssigment){
+
+    hatkid::LongNumber test_123 = hatkid::LongNumber{"123"};
+
+    EXPECT_EQ(test_123, VAL_123);
+    
+}
+
+
+
