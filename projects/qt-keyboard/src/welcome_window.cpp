40f966c7509e4344f147da9e046c7f26ccd19c7f
HatKid <dimno2007@gmail.com>
2026-09-23 20:46:35 +0300

    add text to the welcome window

--- projects/qt-keyboard/src/welcome_window.cpp
+++ projects/qt-keyboard/src/welcome_window.cpp
@@ -1,18 +1,26 @@
 #include <QPushButton>
-#include <QGridLayout>
+#include <QVBoxLayout>
+#include <QLabel>
 
 #include "welcome_window.hpp"
 
 using hatkid::WelcomeWindow;
 
-WelcomeWindow::WelcomeWindow(QWidget* parent) : QWidget(parent){
+WelcomeWindow::WelcomeWindow(QWidget* parent): QWidget(parent){
+
+  QLabel* text = new QLabel("Добро пожаловать в клавиатуру! Она чего-то взгрустнула. Развесели её!", this);
+
   QPushButton* button = new QPushButton("Перейти к клавиатуре",this); 
   button->setSizePolicy(
         QSizePolicy::Fixed,
         QSizePolicy::Fixed
     );
-  QGridLayout* layout = new QGridLayout(this);
+  QVBoxLayout* layout = new QVBoxLayout(this);
+  layout->addWidget(text,0,Qt::AlignCenter);
   layout->addWidget(button,0, Qt::AlignCenter);
   setLayout(layout);
   connect(button, &QPushButton::clicked, this, &WelcomeWindow::keyboardWindowRequested);
+
+  adjustSize();
+
 }
